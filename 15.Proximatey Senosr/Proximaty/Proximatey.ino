841e302b291944604436c761039d622c11a65845
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-06 00:32:20 +0530

    Proximatey Sensor Interface With ESP32

---
+++ 15.Proximatey Senosr/Proximaty/Proximatey.ino
@@ -0,0 +1,20 @@
+/*
+
+Author: Dharmendra Kumar Yadav
+*/
+const int SENSOR_OUTPUT_PIN = 16;  /* Capacitive sensor O/P pin */
+
+void setup() {
+  pinMode(SENSOR_OUTPUT_PIN, INPUT);
+  Serial.begin(9600);             /* Define baud rate for serial communication */
+  delay(1000);                    /* Power On Delay */
+}
+
+void loop() {
+  int sensor_output;
+  sensor_output = digitalRead(SENSOR_OUTPUT_PIN);
+  if( sensor_output == HIGH)  {
+    Serial.print("Object detected\n\n"); 
+  }
+  delay(100);
+}
