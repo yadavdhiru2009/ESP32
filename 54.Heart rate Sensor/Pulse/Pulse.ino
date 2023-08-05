71838b2efd31d334e3004c716aa8aaf1f0f60909
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-06 01:24:15 +0530

    Heart Rate Sensor Interface With ESP32

---
+++ 54.Heart rate Sensor/Pulse/Pulse.ino
@@ -0,0 +1,33 @@
+/*
+Author:Dharmendra Kumar Yadav
+*/
+//  Variables
+int PulseSensor = A0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+int LED = 13;   //  The on-board Arduion LED
+int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore.
+
+// The SetUp Function:
+void setup() 
+  {
+  pinMode(LED,OUTPUT);         // pin that will blink to your heartbeat!
+  Serial.begin(9600);         // Set's up Serial Communication at certain speed.
+ }
+
+// The Main Loop Function
+void loop() {
+
+   Signal = analogRead(PulseSensor);  // Read the PulseSensor's value.
+   Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
+   if(Signal > Threshold)
+      {                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+      digitalWrite(LED,HIGH);
+      }
+    else
+     {
+      digitalWrite(LED,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
+     }
+
+delay(10);
+
+}
