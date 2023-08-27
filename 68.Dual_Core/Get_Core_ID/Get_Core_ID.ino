7e12334015c2298cf614f6a3ee762890cb3ef4c8
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-27 10:04:57 +0530

    Dual Core Programming For ESP32

---
+++ 68.Dual_Core/Get_Core_ID/Get_Core_ID.ino
@@ -0,0 +1,16 @@
+
+/* 
+ *  Author:dharmendra Kumar Yadav
+ 
+ */
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("setup() running on core ");
+  Serial.println(xPortGetCoreID());
+}
+
+void loop() {
+  Serial.print("loop() running on core ");
+  Serial.println(xPortGetCoreID());
+}
